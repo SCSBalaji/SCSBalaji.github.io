1f9717e5e82cd34a66ade58da9ccebc3b65944d2
Chaitanya Satya Balaji Sangineedi <chaitanyasatyabalaji@gmail.com>
2026-01-25 17:55:44 +0530

    Add initial portfolio structure with navigation, background animation, and notification system

---
+++ script.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", function() {
+    let header = document.querySelector('header');
+    let databody = document.querySelector('.databody');
+
+
+    document.body.style.height = header.clientHeight + databody.clientHeight + 'px';
+    
+}, false);
+
+
+window.addEventListener("resize", function() {
+    let header = document.querySelector('header');
+    let databody = document.querySelector('.databody');
+
+    document.body.style.height = header.clientHeight + databody.clientHeight + 'px';
+    
+}, false);
